170af016106cbf75a8416fdd36a9ef8206eb4f10
Jan Maly <jan@invenus.dev>
2025-05-12 14:10:57 +0200

    feat: initial commit

---
+++ window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    
+    def __init__(self, width, height):
+        self.root_widget = Tk()
+        self.root_widget.title("Window")
+        self.canvas = Canvas(self.root_widget, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=True)
+        self.running = False
+        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
+      
+    def redraw(self):
+        self.root_widget.update_idletasks()
+        self.root_widget.update()
+      
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()            
+    
+    def close(self):
+        self.running = False
+        
+    
