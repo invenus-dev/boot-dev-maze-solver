acaee0cb38b4a1e9982b5f19fb1c54f83beb8685
Jan Maly <jan@invenus.dev>
2025-05-12 14:18:26 +0200

    feat: line drawing

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from window import Window
+from line import Line
+from point import Point
+
 
 def main():
-  win = Window(800, 600)
-  win.wait_for_close()
+    win = Window(800, 600)
+
+    win.draw_line(Line(Point(100, 100), Point(200, 200)), "red")
+    win.draw_line(Line(Point(100, 400), Point(500, 400)), "black")
+
+    win.wait_for_close()
+
 
 main()
--- window.py
+++ window.py
@@ -1,7 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
+from line import Line
 
 class Window:
-    
+
     def __init__(self, width, height):
         self.root_widget = Tk()
         self.root_widget.title("Window")
@@ -9,17 +10,18 @@ class Window:
         self.canvas.pack(fill=BOTH, expand=True)
         self.running = False
         self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
-      
+
     def redraw(self):
         self.root_widget.update_idletasks()
         self.root_widget.update()
-      
+
     def wait_for_close(self):
         self.running = True
         while self.running:
-            self.redraw()            
-    
+            self.redraw()
+
     def close(self):
         self.running = False
-        
     
+    def draw_line(self, line: Line, color="black"):
+        line.draw(self.canvas, color)
